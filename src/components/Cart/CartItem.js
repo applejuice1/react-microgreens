12b5d1bef45db53b8b0de17cf4ad14cfff21a752
Applej <antony124442@gmail.com>
2019-09-12 08:58:54 +0300

    paypal

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -43,7 +43,7 @@ export default function CartItem({item,value}) {
             </div>
                 <div className="col-10 mx-auto col-lg-2">
                 <strong> item total : $ {total}</strong>
-                {price}
+                
             </div>
         </div>       
     );
